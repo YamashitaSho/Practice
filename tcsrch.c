1408ade6dab0038e9288080ae714b4264f3b8818
YamashitaSho <YamashitaSho@TsujiTakuya-no-Mac-mini.local>
2016-02-08 17:46:34 +0900

    opptional update

--- tcsrch.c
+++ tcsrch.c
@@ -6,14 +6,17 @@ C言語学習用課題
 該当するものがあれば 検索文字列”xxx”が見つかりました
 該当するものがなければ 検索文字列”xxx”が見つかりませんでした
 
-想定コマンドライン : tcsrch -o "original" -f "matching chars"
+コマンドライン
+-o string:検索対象テキスト
+-f string:検索文字列
+
 
 エラーメッセージ
 ・引数が不正な場合
 ・その他
 
 */
-int argerr(int cargc);				//エラーチェック
+int argchk(char* arg);				//コマンドライン引数の識別
 void errmsg(int p);					//エラーメッセージ表示
 int srch(char* org, char* match);	//検索
 
@@ -21,21 +24,56 @@ int Strlength(const char* s);		//文字列の長さを調べる
 
 
 int main(int argc, char *argv[]){
-	int chk = argerr(argc);			//引数エラーの検出 errに結果を代入
-
-	if (chk == 0) {
-		chk = srch(argv[2], argv[4]);		//検索
+	
+	int chk;						//エラー検出用変数 0でなければエラー
+	int s_org = 0;					//検索対象テキスト指定のオプション
+	int s_find = 0;					//検索文字列指定のオプション
+	
+	for(int i = 1; i < argc; i++){	//1つ目のパラメータは実行ファイルなので弾く
+//	ループ毎に1つずつコマンドラインオプションを取得する：argchk(argv[i])
+		if (argchk(argv[i]) == 15){			//すでに検出されたものでないかチェック
+			if (s_org == 0){
+				s_org = i;					//-oがあった配列番号
+			}else{
+				chk = 1;
+			}
+		}
+		if (argchk(argv[i]) == 6){
+			if (s_find == 0){
+				s_find = i;					//-fがあった配列番号
+			}else{
+				chk = 1;
+			}
+		}
 	}
-	errmsg(chk);
+	
+	printf("s_org:%d\ns_find:%d\n", s_org, s_find);
+	if (chk == 0) {
+		chk = srch(argv[s_org+1], argv[s_find+1]);		//検索
+	}
+	errmsg(chk);					//エラーメッセージを表示
+	return 0;
+}
+
+
+//コマンドライン引数の識別
+int argchk(char* arg){
+	if (arg[0] == '-'){
+		if (arg[1] == 'f'){
+			return 6;
+		} else if (arg[1] == 'o'){
+			return 15;
+		}
+	}else{
+		return 0;
+	}
 	return 0;
 }
 
 
 //検索
 int srch(char* org, char* match){
-	/*
-	方針:1文字目を探す→全一致かどうか確認する
-	*/
+	//	方針:1文字目を探す→全一致かどうか確認する
 	int orglength = Strlength(org);
 	int matchlength = Strlength(match);
 	int scm = orglength - matchlength + 1; //サーチ回数
@@ -60,10 +98,8 @@ int srch(char* org, char* match){
 					srchresult++;	//発見
 					break;
 				}
-//				printf("m%d", j);
 			}
 		}
-//		printf("s%d\n", i);
 	}
 
 	if (srchresult >= 1){
@@ -84,15 +120,6 @@ int Strlength(const char* s){
 		s++; n++;
 	}
 	return (n);
-}
-
-
-//エラー
-int argerr(int cargc){
-	if (cargc != 5) {
-		return 1;					//引数の数が5でない場合エラーコード:1
-	}
-	return 0;
 }
 
 
